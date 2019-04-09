bb9e98bb2147408709a7ff47268be817afbe3021
farid safwan <faridsafwan@aerodyne.group>
2019-04-09 15:22:58 +0800

    first commit

---
+++ src/components/Customer.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+    <div v-if="this.$route.params.name">
+        <h4>Customer</h4>
+        [id: {{ this.$route.params.id }} - 
+        name: {{ this.$route.params.name }}]
+    </div>
+    <div v-else>
+        <br/>
+        <p>Please click on a Customer...</p>
+    </div>
+    <button @click="test()">cuba</button>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: "customer",
+  data() {
+    return {
+      customers: null
+    };
+  },
+    methods: {
+      test(){
+          this.customers = this.$route.params.name
+          console.log(this.customers);
+          
+      }
+  }
+};
+</script>
---
+++ src/components/CustomersList.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="list row">
+        <div class="col-md-6">
+            <h4>Customers List</h4>
+            <ul>
+                <!-- <li v-for="(customer, index) in customers" :key="index">
+                    <router-link :to="{
+                            name: 'customer-details',
+                            params: { name: customer.name, id: index + 1 }
+                        }">
+                            {{customer.name}}
+                    </router-link>
+                </li> -->
+                <li>
+                  <router-link :to="{
+                            name: 'customer-details',
+                            params: { name: 'MLNG SATU', id: 'MLNG1' }
+                        }">
+                            maling1
+                    </router-link>
+                </li>
+                <li>
+                  <router-link :to="{
+                            name: 'customer-details',
+                            params: { name: 'MLNG DUA', id: 'MLNG2' }
+                        }">
+                            maling2
+                    </router-link>
+                </li>
+            </ul>
+        </div>
+        <div class="col-md-6">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "customers-list",
+  data() {
+    return {
+      customers: [
+        { name: "Jack Smith" },
+        { name: "Adam Johnson" },
+        { name: "Katherin Watson" }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+.list {
+  text-align: left;
+  max-width: 500px;
+  margin: auto;
+}
+</style>
